e32923301724d5b39b0aa459944d05afbd4a50ce
Frank Failla <ffailla@gmail.com>
2011-01-04 10:26:39 -0500

    added cdt and jisql build tasks to sync-restore

--- sync-emacs.sh
+++ sync-emacs.sh
@@ -2,6 +2,9 @@
 
 if [ "$1" == "--restore" ] 
 then
+  set p=pwd
+  cd ./.emacs.d/site-lisp/cdt/ && lein deps, install && cd $p 
+  ant -f ./.emacs.d/site-lisp/jisql/build.xml
   rsync -av --delete ./.emacs ~/
   rsync -av --delete ./.emacs.d ~/
   exit 0;
