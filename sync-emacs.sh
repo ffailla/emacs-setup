82188720f048978be8bc1da241f30465caf376a0
Frank Failla <ffailla@gmail.com>
2011-01-07 13:37:59 -0500

    fixed lein call in sync script

--- sync-emacs.sh
+++ sync-emacs.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "--restore" ] 
 then
   set p=pwd
-  cd ./.emacs.d/site-lisp/cdt/ && lein deps, install && cd $p 
+  cd ./.emacs.d/site-lisp/cdt/ && lein deps install && cd $p 
   ant -f ./.emacs.d/site-lisp/jisql/build.xml
   rsync -av --delete ./.emacs ~/
   rsync -av --delete ./.emacs.d ~/
