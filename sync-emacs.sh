9103e56794b4c5e2c6b0cf5a4234ff9c35343596
Frank Failla <ffailla@gmail.com>
2011-02-28 23:16:09 -0500

    added trailing slash to dirs in rsyncs

--- sync-emacs.sh
+++ sync-emacs.sh
@@ -6,13 +6,13 @@ then
   cd ./.emacs.d/site-lisp/cdt/ && lein deps install && cd $p 
   ant -f ./.emacs.d/site-lisp/jisql/build.xml
   rsync -av --delete ./.emacs ~/
-  rsync -av --delete ./.emacs.d ~/
-  rsync -av --delete ./bin ~/
+  rsync -av --delete ./.emacs.d/ ~/
+  rsync -av --delete ./bin/ ~/
   rsync -av --delete ./.screenrc ~/
   exit 0;
 fi
 
 rsync -av --copy-dirlinks --delete ~/.emacs ./
-rsync -av --copy-dirlinks --delete ~/.emacs.d ./
-rsync -av --copy-dirlinks --delete ~/bin ./
+rsync -av --copy-dirlinks --delete ~/.emacs.d/ ./
+rsync -av --copy-dirlinks --delete ~/bin/ ./
 rsync -av --copy-dirlinks --delete ~/.screenrc ./
