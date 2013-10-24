3a7f9c99329a37fbba0e80b9bc2b4478f406159c
Frank Failla <ffailla@gmail.com>
2013-10-24 05:02:24 -0400

    removed .terminfo from repo

--- sync-emacs.sh
+++ sync-emacs.sh
@@ -5,18 +5,12 @@ git submodule update
 
 if [ "$1" == "--restore" ] 
 then  
-  #cp ./.emacs ~/
   rsync -av --delete ./.emacs.d/ ~/.emacs.d
   #rsync -av --delete ./bin/ ~/bin
   cp ./.screenrc ~/  
-  cp ./.terminfo ~/
-  #ant -f ./.emacs.d/vendor/jisql/build.xml
-  #cd ./.emacs.d/vendor/cdt/ && lein deps install
   exit 0;
 fi
 
-#rsync -av --copy-dirlinks --delete ~/.emacs ./
 rsync -av --copy-dirlinks --delete ~/.emacs.d/ ./.emacs.d
 #rsync -av --copy-dirlinks --delete ~/bin/ ./bin
 rsync -av --copy-dirlinks --delete ~/.screenrc ./
-rsync -av --copy-dirlinks --delete ~/.terminfo ./
