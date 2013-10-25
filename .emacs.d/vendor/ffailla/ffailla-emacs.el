ea98bebd87348fe3b119dc57e1219147a1d575bc
Frank Failla <ffailla@gmail.com>
2013-10-25 14:36:29 -0400

    commented out cygwin support

--- .emacs.d/vendor/ffailla/ffailla-emacs.el
+++ .emacs.d/vendor/ffailla/ffailla-emacs.el
@@ -38,20 +38,20 @@
 ;;;
 ;;; windows specific configuration
 ;;;
-(if (eq system-type 'windows-nt)
-  (setenv "PATH" (concat "c:/cygwin/bin;" (getenv "PATH")))
-  (setq exec-path (cons "c:/cygwin/bin/" exec-path))
-  (require 'cygwin-mount)
-  (cygwin-mount-activate)
+;; (if (eq system-type 'windows-nt)
+;;   (setenv "PATH" (concat "c:/cygwin/bin;" (getenv "PATH")))
+;;   (setq exec-path (cons "c:/cygwin/bin/" exec-path))
+;;   (require 'cygwin-mount)
+;;   (cygwin-mount-activate)
 
-  (add-hook 'comint-output-filter-functions 'shell-strip-ctrl-m nil t)
-  (add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt nil t)
-  (setq explicit-shell-file-name "bash.exe")
-  ;; For subprocesses invoked via the shell
-  ;; (e.g., "shell -c command")
-  (setq shell-file-name explicit-shell-file-name)
+;;   (add-hook 'comint-output-filter-functions 'shell-strip-ctrl-m nil t)
+;;   (add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt nil t)
+;;   (setq explicit-shell-file-name "bash.exe")
+;;   ;; For subprocesses invoked via the shell
+;;   ;; (e.g., "shell -c command")
+;;   (setq shell-file-name explicit-shell-file-name)
   
-  )
+;;   )
 
 ;;;
 ;;; linum settings
